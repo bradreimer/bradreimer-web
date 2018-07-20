20cbd0e9aee8bfba75a8a11959b225bf932f0795
Brad Reimer <breimer@dccnet.com>
2018-07-19 21:22:38 -0700

    Apply editorconfig style to generated code

--- SchrodyWebApp/Pages/Error.cshtml.cs
+++ SchrodyWebApp/Pages/Error.cshtml.cs
@@ -8,16 +8,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SchrodyWebApp.Pages
 {
-    public class ErrorModel : PageModel
-    {
-        public string RequestId { get; set; }
+	public class ErrorModel : PageModel
+	{
+		public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public void OnGet()
+		{
+			RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+		}
+	}
 }
--- SchrodyWebApp/Pages/Privacy.cshtml.cs
+++ SchrodyWebApp/Pages/Privacy.cshtml.cs
@@ -7,10 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SchrodyWebApp.Pages
 {
-    public class PrivacyModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class PrivacyModel : PageModel
+	{
+		public void OnGet()
+		{ }
+	}
 }
