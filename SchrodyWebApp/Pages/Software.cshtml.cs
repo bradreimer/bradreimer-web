f9cf13a7026b561959bb45aac4ec11f326648623
Brad Reimer <breimer@dccnet.com>
2018-08-03 23:35:23 -0700

    Add Fletch and Fibs to main page
    Added cards for Fibs and Fletch which point to Instagram pages.

---
+++ SchrodyWebApp/Pages/Software.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SchrodyWebApp.Pages
+{
+	public class SoftwareModel : PageModel
+	{
+		public void OnGet()
+		{
+			//
+		}
+	}
+}
