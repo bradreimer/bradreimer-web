27103cba80552557431eb329db5151a9320a6466
Brad Reimer <breimer@ea.com>
2020-05-24 22:11:29 -0700

    Restore razor formatting
    - Use Blazorise to install bootstrap and fontawesome
    - Flip navigation to top

--- SchrodyWebApp/Program.cs
+++ SchrodyWebApp/Program.cs
@@ -1,25 +1,42 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace SchrodyWebApp
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services
+				.AddBlazorise(options =>
+				{
+					options.ChangeTextOnKeyPress = true;
+				})
+				.AddBootstrapProviders()
+				.AddFontAwesomeIcons();
 
-            await builder.Build().RunAsync();
-        }
-    }
+			builder.Services.AddSingleton(new HttpClient
+			{
+				BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+			});
+
+			builder.RootComponents.Add<App>("app");
+
+			var host = builder.Build();
+
+			host.Services
+				.UseBootstrapProviders()
+				.UseFontAwesomeIcons();
+
+			await host.RunAsync();
+		}
+	}
 }
