18d547140135ab886823e21adf4490100ce31506
Brad Reimer <breimer@ea.com>
2021-06-13 21:26:52 -0700

    Add sayHello method w/o backend storage
    SayHello will increment a number, but it will
    reset when the function app resource is
    unloaded

--- api/SayHello.cs
+++ api/SayHello.cs
@@ -8,26 +8,33 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Schrody.Placeholder
+namespace Schrody
 {
-    public static class PlaceholderMessage
+    public static class SayHello
     {
-        [FunctionName("PlaceholderMessage")]
+		private static int s_fletch;
+		private static int s_fibs;
+
+        [FunctionName("SayHello")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+			int count = name switch
+			{
+				"Fletch" => ++s_fletch,
+				"Fibs" => ++s_fibs,
+				_ => 0
+			};
+
+			string responseMessage =
+				$"<strong>Hello human!</strong> ${count} people have said hello to me.";
 
             return new OkObjectResult(responseMessage);
         }
